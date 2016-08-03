8cb9ce27beeaa5fdc35abb3bfa61add762cd44cc
Justin Beuck <jbeuck@yahoo.com>
2016-08-02 21:39:32 -0500

    added app.js and passport to applicaiton

---
+++ app.js
@@ -0,0 +1,17 @@
+var express 		 = require('express');
+var path 				 = require('path');
+var favicon 		 = require('serve-favicon');
+var logger 			 = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser 	 = require('body-parser');
+var passport 	 	 = require('passport');
+
+require('./app_api/models/db');
+require('./app_api/config/passport');
+
+var routesApi = require('./app_api/routes/index');
+
+var app = express();
+
+app.use(passport.initialize());
+app.use('/api', routesApi);
