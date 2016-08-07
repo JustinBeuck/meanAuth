5db8329edeb88ca0fe87efbc6be3165a9c81522f
Justin Beuck <jbeuck@yahoo.com>
2016-08-06 23:16:38 -0500

    isloggedin function added correctly changed spacing

--- app_client/common/services/authentication.service.js
+++ app_client/common/services/authentication.service.js
@@ -23,4 +23,19 @@
 				logout : logout
 			};
 		}
+		
+		var isLoggedIn = function() {
+	  var token = getToken();
+	  var payload;
+
+	  if(token){
+	    payload = token.split('.')[1];
+	    payload = $window.atob(payload);
+	    payload = JSON.parse(payload);
+
+	    return payload.exp > Date.now() / 1000;
+	  } else {
+	    return false;
+	  }
+	};
 })();
